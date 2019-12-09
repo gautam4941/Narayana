b3c9f8ceb2a80e0df75bd91a26d6673ce3ccc67a
Kumar Gautam Pathak <36535562+gautam4941@users.noreply.github.com>
2019-12-09 13:56:06 +0530

    Create Convert_Xlsx_to_csv.py

---
+++ Convert_Xlsx_to_csv.py
@@ -0,0 +1,19 @@
+def convert_xlsx_to_csv( path1, path2 ):
+    book1xlsx = pd.read_excel( path1 )
+    col = book1xlsx.columns
+    
+    data = []
+    for i in range( len( book1xlsx.index ) ):
+        temp = []
+
+        for j in col:
+            temp.append( book1xlsx[j][i] )
+
+        data.append( temp )
+
+    pd.DataFrame( data ).to_csv( path2 )
+
+    
+path1 = r"Excel Data/Book1.xlsx"
+path2 = r"Excel Data/Converted_csv.csv"
+convert_xlsx_to_csv( path1, path2 )
